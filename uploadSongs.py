12a34cc5ec759ebb33ef4f87153752effdefa147
Rowzein <rowzen60@gmail.com>
2020-12-02 13:53:27 +0545

    add upload songs file for client

---
+++ uploadSongs.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('sh ~/adv/uploadSongs.sh');
---
+++ uploadSongs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+if [ -d "/media/adminpc/BUZZMEDIA/buzz_video_songs/" ] ; then
+
+	pkill -f chromium-browser
+
+	cd /home/adminpc/lampstack-7.1.20-1/apache2/htdocs/client/uploads/ && rm -rf video_songs 
+
+	cp -R /media/adminpc/BUZZMEDIA/buzz_video_songs /home/adminpc/lampstack-7.1.20-1/apache2/htdocs/client/uploads/video_songs/
+
+	/home/adminpc/lampstack-7.1.20-1/php/bin/php /home/adminpc/lampstack-7.1.20-1/apache2/htdocs/client/artisan upload:songs
+
+	/home/adminpc/lampstack-7.1.20-1/php/bin/php /home/adminpc/lampstack-7.1.20-1/apache2/htdocs/client/artisan config:cache
+
+	/home/adminpc/lampstack-7.1.20-1/php/bin/php /home/adminpc/lampstack-7.1.20-1/apache2/htdocs/client/artisan view:clear
+
+	init 6
+
+else
+	echo "not found"
+
+fi
+
